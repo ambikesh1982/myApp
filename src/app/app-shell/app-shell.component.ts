b976a85d1d7154f40fe3343f5b29ed4ec46311fb
ambikesh1982 <ambikesh1982@gmail.com>
2020-02-07 19:42:20 -0500

    sidenav width fix

--- src/app/app-shell/app-shell.component.ts
+++ src/app/app-shell/app-shell.component.ts
@@ -10,6 +10,19 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class AppShellComponent {
 
+  navList = [
+    { menuIcon: 'home', menuName: 'Home', menuRoute: '/' },
+    { menuIcon: 'assignment', menuName: 'My Orders', menuRoute: '/checkout' },
+    { menuIcon: 'shopping_cart', menuName: 'Cart', menuRoute: '/cart' },
+    { menuIcon: 'favorite', menuName: 'Wish List', menuRoute: '/wishlist' },
+    { menuIcon: 'account_circle', menuName: 'Profile', menuRoute: '/user' },
+    { menuIcon: 'language', menuName: 'Language', menuRoute: './' },
+    { menuIcon: 'android', menuName: 'Download App', menuRoute: './' },
+    { menuIcon: 'help', menuName: 'Help', menuRoute: './' },
+    { menuIcon: 'feedback', menuName: 'Feedback', menuRoute: './' },
+
+  ];
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
